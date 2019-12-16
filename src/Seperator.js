cc3e196da5a1bc8d6dff696fbff237573d2350f6
Deepak Sisodiya <deepak.sisodiya@gmail.com>
2019-12-16 13:58:04 +0530

    making it dragable

--- src/Seperator.js
+++ src/Seperator.js
@@ -11,11 +11,12 @@ export default class SeperatorNode extends Node {
       // here you have to specify all values that can be stored in this node
       group: "block",
       selectable: false,
+      draggable: true,
       // parseDOM and toDOM is still required to make copy and paste work
       parseDOM: [
         {
           tag: this.name,
-          getAttrs: () => { }
+          getAttrs: () => {}
         }
       ],
       toDOM: () => [
@@ -37,7 +38,7 @@ export default class SeperatorNode extends Node {
     return {
       props: ["node", "updateAttrs", "view"],
       template: `
-        <div style="text-align:center">
+        <div @paste.stop style="text-align:center">
           <span class="dot"></span>
           <span class="dot"></span>
           <span class="dot"></span>
