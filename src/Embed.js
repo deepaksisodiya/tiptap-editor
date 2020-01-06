8e299053da97a91012a6a4873cf512ae6fcb5474
Ujjwal Mishra <ujjwal@scroll.in>
2020-01-06 18:33:20 +0530

    add embed website restrictions

--- src/Embed.js
+++ src/Embed.js
@@ -79,7 +79,23 @@ export default class EmbedNode extends Node {
       methods: {
         onClickButton() {
           if (!this.src) return;
-          this.isIframeLoaded = true;
+          const isLinkSouported = Boolean(this.parseVideo(this.src).type);
+          if (isLinkSouported) this.isIframeLoaded = true;
+        },
+        parseVideo(url) {
+          let type = null;
+          url.match(
+            /(http:|https:|)\/\/(player.|www.)?(vimeo\.com|youtu(be\.com|\.be|be\.googleapis\.com))\/(video\/|embed\/|watch\?v=|v\/)?([A-Za-z0-9._%-]*)(&\S+)?/
+          );
+          if (RegExp.$3.indexOf("youtu") > -1) {
+            type = "youtube";
+          } else if (RegExp.$3.indexOf("vimeo") > -1) {
+            type = "vimeo";
+          }
+          return {
+            type: type,
+            id: RegExp.$6
+          };
         }
       },
       template: `
