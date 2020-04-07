c07142795e2f1cd027962c1117cbef710a6033dd
Deepak Sisodiya <deepak.sisodiya@gmail.com>
2020-04-07 13:52:02 +0530

    fix title issue

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -281,6 +281,11 @@ export default {
       type: Boolean,
       required: false,
       default: () => false
+    },
+    hideTitleError: {
+      type: Function,
+      required: false,
+      default: Function.prototype
     }
   },
   components: {
@@ -290,7 +295,6 @@ export default {
   },
   data() {
     return {
-      displayTitleError: this.showTitleError,
       shouldShowTooltip: !localStorage.getItem("editorTour"),
       data: this.content,
       imageSrc: "",
@@ -488,13 +492,10 @@ export default {
       this.shouldShowTooltip = false;
     },
     closeTitleError() {
-      this.displayTitleError = false;
+      this.hideTitleError();
     }
   },
   watch: {
-    showTitleError() {
-      this.displayTitleError = this.showTitleError;
-    },
     content(newValue) {
       if (newValue) {
         const newContent = this.addTitle(newValue, this.title);
@@ -525,6 +526,9 @@ export default {
         this.editor.view.state.doc,
         this.editor.schema.nodes.lock
       );
+    },
+    displayTitleError() {
+      return this.showTitleError;
     }
   }
 };
