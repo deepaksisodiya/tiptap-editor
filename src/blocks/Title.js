0b26fca3ae10fd0d494beb59668bbbe961eca2e1
Ujjwal Mishra <ujjwal@scroll.in>
2020-04-09 15:22:27 +0530

    fix editing experience

--- src/blocks/Title.js
+++ src/blocks/Title.js
@@ -1,4 +1,5 @@
 import { Node } from "tiptap";
+import { TextSelection } from "tiptap";
 
 export default class Title extends Node {
   get name() {
@@ -15,6 +16,44 @@ export default class Title extends Node {
       ],
       toDOM: () => ["h1", 0],
       marks: "bold italic"
+    };
+  }
+  keys() {
+    return {
+      Enter: (state, dispatch) => {
+        let {
+          tr,
+          selection: { anchor }
+        } = state;
+        if (tr.doc.resolve(anchor).parent.type.name !== "title") return false;
+        let textSelection = TextSelection.create(
+          tr.doc,
+          anchor + 3,
+          anchor + 3
+        );
+        return dispatch(tr.setSelection(textSelection));
+      },
+      Backspace: (state, dispatch) => {
+        let {
+          tr,
+          selection: { anchor }
+        } = state;
+        const currentNode = tr.doc.resolve(anchor).parent.type.name;
+
+        if (
+          currentNode !== "header" &&
+          currentNode !== "title" &&
+          tr.doc.resolve(anchor - 3).parent.type.name === "header"
+        ) {
+          let textSelection = TextSelection.create(
+            tr.doc,
+            anchor - 4,
+            anchor - 4
+          );
+          return dispatch(tr.setSelection(textSelection));
+        }
+        return false;
+      }
     };
   }
 }
