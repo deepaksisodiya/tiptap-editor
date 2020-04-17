1a63356e28347ff2ace2146e6f10a2e6278974e6
Ujjwal Mishra <ujjwal@scroll.in>
2020-04-17 11:41:10 +0530

    fix embed json

--- src/blocks/embed/Embed.js
+++ src/blocks/embed/Embed.js
@@ -1,4 +1,5 @@
 import { Node } from "tiptap";
+import { Fragment } from "prosemirror-model";
 
 import Embed from "./Embed.vue";
 
@@ -75,6 +76,43 @@ export default class EmbedNode extends Node {
 
   commands({ type }) {
     return attrs => (state, dispatch) => {
+      const node = type.create(attrs);
+      node.__proto__.toJSON = function() {
+        let obj = { type: this.type.name };
+        // eslint-disable-next-line no-unused-vars
+        for (let _ in this.attrs) {
+          obj.attrs = this.attrs;
+          break;
+        }
+        if (this.content.size) obj.content = this.content.toJSON();
+        if (this.marks.length) obj.marks = this.marks.map(n => n.toJSON());
+        if (this.type.name === "embed") {
+          obj.content = {
+            type: "html",
+            html: this.attrs.html
+          };
+        }
+        return obj;
+      };
+      node.__proto__.fromJSON = function(schema, json) {
+        if (!json) throw new RangeError("Invalid input for Node.fromJSON");
+        let marks = null;
+        if (json.marks) {
+          if (!Array.isArray(json.marks))
+            throw new RangeError("Invalid mark data for Node.fromJSON");
+          marks = json.marks.map(schema.markFromJSON);
+        }
+        if (json.type == "text") {
+          if (typeof json.text != "string")
+            throw new RangeError("Invalid text node in JSON");
+          return schema.text(json.text, marks);
+        }
+        let content = Fragment.fromJSON(
+          schema,
+          json.type === "embed" ? json.content : undefined
+        );
+        return schema.nodeType(json.type).create(json.attrs, content, marks);
+      };
       return dispatch(state.tr.replaceSelectionWith(type.create(attrs)));
     };
   }
