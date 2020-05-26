597a6dfb065847fd4fea0973ae984198f227156e
Deepak Sisodiya <deepak.sisodiya@gmail.com>
2020-05-26 15:33:18 +0530

    making a Demo component to show on github page

---
+++ src/Demo.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <Article
+      :onUpdatePost="onUpdatePost"
+      :content="content"
+      :uploadImage="uploadImage"
+      :getEmbeds="getEmbeds"
+    />
+    <div>Title is {{ title }}</div>
+    <vue-json-pretty :path="'res'" :data="blocks"> </vue-json-pretty>
+  </div>
+</template>
+
+<script>
+import "@/assets/scss/base.scss";
+import "@/assets/scss/editor.scss";
+import "@/assets/scss/article.scss";
+import VueJsonPretty from "vue-json-pretty";
+import Article from "./components/Article.vue";
+import { uploadImage, getEmbeds } from "./api";
+
+export default {
+  name: "Demo",
+  data() {
+    return {
+      title: "",
+      blocks: {}
+    };
+  },
+  components: {
+    Article,
+    VueJsonPretty
+  },
+  methods: {
+    uploadImage,
+    getEmbeds,
+    onUpdatePost({ blocks, title }) {
+      this.blocks = blocks;
+      this.title = title;
+    }
+  }
+};
+</script>
