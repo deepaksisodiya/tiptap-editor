3f82f2a1b8f34865fad424a90b8b1432be4ef82e
Deepak Sisodiya <deepak.sisodiya@gmail.com>
2020-08-05 16:15:56 +0530

    basic audio template

--- src/components/blocks/audio/Audio.vue
+++ src/components/blocks/audio/Audio.vue
@@ -1,11 +1,52 @@
 <template>
-  <div>
-    Audio component
-  </div>
+  <figure>
+    <audio controls="controls" :src="data" />
+    <figcaption>
+      <input
+        v-model="caption"
+        placeholder="Type caption for Audio (optional)"
+        @paste.stop
+      />
+    </figcaption>
+  </figure>
 </template>
 
 <script>
 export default {
-  name: "Audio"
+  name: "Audio",
+  props: ["node", "updateAttrs", "view", "getPos", "options"],
+  data() {
+    return {
+      data: this.node.attrs.src
+    };
+  },
+  computed: {
+    src: {
+      get() {
+        return this.node.attrs.src;
+      },
+      set(src) {
+        this.updateAttrs({
+          src
+        });
+      }
+    },
+    caption: {
+      get() {
+        return this.node.attrs.caption;
+      },
+      set(caption) {
+        this.updateAttrs({
+          caption
+        });
+      }
+    }
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.view.focus();
+      this.$el.scrollIntoView(true);
+    });
+  }
 };
 </script>
