637b601d259583bdec1425154a224cbc380fccfd
Ujjwal Mishra <ujjwal@scroll.in>
2020-08-13 15:46:43 +0530

    Revert "Revert "check cursor posion in a node""
    This reverts commit acc8d26d57698b606f5b78c186b6a17e44927af2.

--- src/components/blocks/Title.js
+++ src/components/blocks/Title.js
@@ -37,7 +37,7 @@ export default class Title extends Node {
           if (
             browser.ios &&
             anchorAfterEnterKeydown - anchorAtEnterKeydown === 2 &&
-            tr.doc.nodeAt(anchorAfterEnterKeydown).textContent
+            !tr.doc.resolve(anchorAtEnterKeydown).parentOffset
           ) {
             let textSelection = TextSelection.create(
               tr.doc,
